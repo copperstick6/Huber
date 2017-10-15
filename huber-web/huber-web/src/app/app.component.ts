9b385c37e172cb20c2f7f2ec6136d526e8d6a6ac
hemank101 <hemank101@gmail.com>
2017-10-15 03:12:11 -0400

    Update UI and JSON

--- huber-web/huber-web/src/app/app.component.ts
+++ huber-web/huber-web/src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-import { MatToolbar } from '@angular/material'; 
+import { Component, OnInit } from '@angular/core';
+import { MatToolbar, MatGridList, MatGridTile } from '@angular/material'; 
+import { RecpCardComponent } from './recp-card/recp-card.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  name = 'Megan';
+export class AppComponent implements OnInit {
+
+  // cards : RecpCardComponent[];
+
+  constructor(private http: HttpClient){
+  
+  }
+  
+  ngOnInit(): void {
+    this.http.get('http://1094eb34.ngrok.io/getUserChoices').subscribe(data => {
+      console.log(JSON.parse(JSON.stringify(data)));
+    });
+  }
+  
+  cards = [ 
+      new RecpCardComponent("Megan", 1000, "Tomatoes, onions, and garlic"),
+      new RecpCardComponent("William", 2010, "Chocolate, celery, ketchup"),
+      new RecpCardComponent("Kevin", 1000000, "Rotten cabbage, wasabi, rice"),
+      new RecpCardComponent("Kevin", 1000100, "Rotten cabbage, wasabi, rice")
+    ];
+ 
 }
--- huber-web/huber-web/src/app/recp-card/recp-card.component.ts
+++ huber-web/huber-web/src/app/recp-card/recp-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatToolbar, MatCardModule } from '@angular/material'; 
 
 @Component({
   selector: 'recp-card',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recp-card.component.css']
 })
 export class RecpCardComponent implements OnInit {
+  name: string;
+  calorie: number;
+  ingredients: string;
 
-  constructor() { }
+  constructor(nameI: string, numberI: number, ingredientsI: string) {
+    this.name = nameI;
+    this.calorie = numberI;
+    this.ingredients = ingredientsI;
+  }
 
   ngOnInit() {
+
   }
+
+
 
 }
